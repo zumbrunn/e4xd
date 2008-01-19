4eb1d1d31fa49ab24c433acfdaeaa101b388bf13
chris.zumbrunn <chris.zumbrunn@c812b93a-3843-0410-81cf-ff7f0148a606>
2008-01-19 15:14:04 +0000

    finished access list implementation and made some minor additions to the view rendering process

---
+++ trunk/objectengine/Mocha/page.js
@@ -0,0 +1,11 @@
+
+/**
+ * Renders the default page template
+ */
+function renderPage() {
+    res.writeln('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"\n\
+       "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">');
+    res.write(this.views.page.toXMLString()
+        .replace(/<textarea(.*)\/>/g,'<textarea$1></textarea>')
+        .replace(/<script(.*)\/>/g,'<script$1></script>'));
+}
